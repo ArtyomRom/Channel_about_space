58129c1b8b439a13c45a616476e39916989bf63f
Artyom <zik21b@gmail.com>
2024-11-30 18:48:47 +0300

    Откорректировал название функции и переменной

--- tg_bot.py
+++ tg_bot.py
@@ -2,18 +2,18 @@ import os
 import requests
 
 def send_photo(link_tg_channel, tg_bot_api):
-    url_send_photo = f'https://api.telegram.org/bot{tg_bot_api}/sendPhoto'
+    photo_url = f'https://api.telegram.org/bot{tg_bot_api}/sendPhoto'
     data = {'chat_id': link_tg_channel}
 
     for filename in os.listdir('images'):
         if not filename.endswith('.txt'):
             with open(f'images/{filename}', 'rb') as image:
-                data['caption'] = send_text(filename)
+                data['caption'] = get_text(filename)
                 file = {'photo': image}
-                requests.post(url_send_photo, files=file, data=data)
+                requests.post(photo_url, files=file, data=data)
         break
 
-def send_text(filename):
+def get_text(filename):
     base_name = filename.split('.')[0]
     text_file_path = f'{base_name}_text.txt'
     if text_file_path in os.listdir('images'):
