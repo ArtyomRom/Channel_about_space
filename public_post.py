f973d35971890c6d61bcb8e1a39f7bdc13f52c75
Artyom <zik21b@gmail.com>
2024-12-15 16:26:42 +0300

    Убрал функцию load_dotenv
    -Вызывается лишь в файле main.py в функции main

--- public_post.py
+++ public_post.py
@@ -1,11 +1,9 @@
 from tg_bot import send_photos
-from dotenv import load_dotenv
 import argparse
 import time
 import os
 
 def public_photos():
-    load_dotenv()
     parser = argparse.ArgumentParser(
         description='Публикация фото в телеграмм канал'
     )
